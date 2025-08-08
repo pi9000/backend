3420ced18403978c5cbf41b971218d6221041927
pi9000 <revify99@gmail.com>
2025-08-08 20:22:23 +0700

    Update account_list.blade.php

--- resources/views/page/member/account_list.blade.php
+++ resources/views/page/member/account_list.blade.php
@@ -22,110 +22,121 @@
                 <th> Last Login Time </th>
                 <th> Last Login IP</th>
                 <th> Player Remark </th>
+                <th> Balance </th>
                 <th> Edit </th>
             </tr>
         </thead>
         <tbody style="text-align:center;color:black">
             @foreach ($data->data as $item)
-            <tr>
-                <td>
-                    {{ $loop->iteration }}
-                </td>
-                <td>
-                    {{ $item->extplayer }}
-                </td>
+                <tr>
+                    <td>
+                        {{ $loop->iteration }}
+                    </td>
+                    <td>
+                        {{ $item->extplayer }}
+                    </td>
 
-                <td>
-                    <span id="password_{{ $item->extplayer }}_display">
-                        <a href='member_details/{{ $item->extplayer }}' target="_blank">
-                            <span class="">
-                            </span>
-                            <span>
-                                <span id='uname_{{ $item->extplayer }}' class="">{{ $item->username }}</span>
-                            </span>
-                            <span class="">
+                    <td>
+                        <span id="password_{{ $item->extplayer }}_display">
+                            <a href='member_details/{{ $item->extplayer }}' target="_blank">
+                                <span class="">
+                                </span>
+                                <span>
+                                    <span id='uname_{{ $item->extplayer }}' class="">{{ $item->username }}</span>
+                                </span>
+                                <span class="">
+                            </a>
+                            <br>
+                            <i onclick="Copytext('uname_{{ $item->extplayer }}')" class="far fa-copy pointer"
+                                style="font-size:15px;" aria-hidden="true" title="Copy"></i>
+                        </span>
+                        <div id="password_{{ $item->extplayer }}" style="display:none">
+                            {{ $item->username }}
+                        </div>
+
+                    </td>
+                    <td class="text-middle"> </td>
+                    <td>
+                        <span id="status_{{ $item->extplayer }}_display">{{ $item->status == 1 ? 'On' : 'Off' }}</span>
+                        <select id="status_{{ $item->extplayer }}" style="display:none" class="w-100">
+                            <option value="1" selected>On</option>
+                            <option value="0">Off</option>
+                        </select>
+                    </td>
+
+                    <td>
+                        <span
+                            id="suspend_{{ $item->extplayer }}_display">{{ $item->status_game == 1 ? 'On' : 'Off' }}</span>
+
+                        <select id="suspend_{{ $item->extplayer }}" style="display:none" class="w-100">
+                            <option value="1">On</option>
+                            <option value="0">Off</option>
+                        </select>
+                    </td>
+
+                    <td>
+                        <span
+                            id="verified_{{ $item->extplayer }}_display">{{ $item->verified == 1 ? 'Yes' : 'No' }}</span>
+
+                        <select id="verified_{{ $item->extplayer }}" style="display:none" class="w-100">
+                            <option value="1">Yes</option>
+                            <option value="0">No</option>
+                        </select>
+                    </td>
+
+                    <td>
+                        <span id="firstname_{{ $item->extplayer }}_display">{{ $item->nama_lengkap }}</span>
+                        <input type="text" id="firstname_{{ $item->extplayer }}" name="player_firstname"
+                            class="w-100" style="display:none" value="{{ $item->nama_lengkap }}">
+                    </td>
+
+                    <td> <span id="mobile_{{ $item->extplayer }}_display">{{ $item->no_hp }}</span>
+                        <input type="mobile" id="mobile_{{ $item->extplayer }}" class="w-100" style="display:none"
+                            value="{{ $item->no_hp }}">
+                        <input class="w-100" id="old_mobile_{{ $item->extplayer }}" type="text" name="old_mobile"
+                            value="{{ $item->no_hp }}" hidden>
+                    </td>
+                    <td class="text-middle"> {{ $item->nama_bank }} </td>
+                    <td class="text-middle"> {{ number_format($item->balance, 2) }} </td>
+                    <td class="text-middle">
+                        {{ $item->created_at ? date('Y-m-d H:i:s', strtotime($item->created_at)) : '' }} </td>
+                    <td class="text-middle">
+                        {{ $item->first_deposit_date ? date('Y-m-d H:i:s', strtotime($item->first_deposit_date)) : '' }}
+                    </td>
+                    <td class="text-middle">
+                        {{ $item->last_deposit_date ? date('Y-m-d H:i:s', strtotime($item->last_deposit_date)) : '' }}
+                    </td>
+                    <td class="text-middle">
+                        {{ $item->last_login_time ? date('Y-m-d H:i:s', strtotime($item->last_login_time)) : '' }}
+                    </td>
+                    <td class="text-middle"> {{ $item->last_login_ip }} </td>
+                    <td class="text-middle"> {{ $item->remark }} </td>
+                    <td>
+                        <a href="javascript:void(0)" class="waves-effect waves-light btn btn-primary btn-sm btn-rounded"
+                            onclick="balance_settings(this, `{{ $item->extplayer }}`, `{{ $item->username }}` )">
+                            &#9998; </a>
+                    </td>
+                    <td>
+                        <a href="javascript:void(0)" class="waves-effect waves-light btn btn-warning btn-sm btn-rounded"
+                            onclick="edit_button(this, `{{ $item->extplayer }}` )"> &#9998;
                         </a>
-                        <br>
-                        <i onclick="Copytext('uname_{{ $item->extplayer }}')" class="far fa-copy pointer"
-                            style="font-size:15px;" aria-hidden="true" title="Copy"></i>
-                    </span>
-                    <div id="password_{{ $item->extplayer }}" style="display:none">
-                        {{ $item->username }}
-                    </div>
 
-                </td>
-                <td class="text-middle"> </td>
-                <td>
-                    <span id="status_{{ $item->extplayer }}_display">{{ $item->status == 1 ? 'On' : 'Off' }}</span>
-                    <select id="status_{{ $item->extplayer }}" style="display:none" class="w-100">
-                        <option value="1" selected>On</option>
-                        <option value="0">Off</option>
-                    </select>
-                </td>
+                        <div style="display:none">
+                            <a href="javascript:void(0)"
+                                class=" waves-effect waves-light btn btn-success btn-sm btn-rounded"
+                                onclick="submit_button(this , `{{ $item->extplayer }}` )"> &#10004; </a>
+                            <a href="javascript:void(0)"
+                                class="cancel_button waves-effect waves-light btn btn-danger btn-sm btn-rounded"
+                                onclick="cancel_button(this, `{{ $item->extplayer }}` )"> &#10007; </a>
 
-                <td>
-                    <span id="suspend_{{ $item->extplayer }}_display">{{ $item->status_game == 1 ? 'On' : 'Off'}}</span>
+                        </div>
+                        <a href="javascript:void(0)" data-toggle="modal"
+                            class="waves-effect waves-light btn btn-sm btn-block btn-rounded"
+                            onclick="showModal('account_password_edit?id={{ $item->id }}&agent_id={{ $item->extplayer }}')">Edit
+                            Password</a>
+                    </td>
 
-                    <select id="suspend_{{ $item->extplayer }}" style="display:none" class="w-100">
-                        <option value="1">On</option>
-                        <option value="0">Off</option>
-                    </select>
-                </td>
-
-                <td>
-                    <span id="verified_{{ $item->extplayer }}_display">{{ $item->verified == 1 ? 'Yes' : 'No'
-                        }}</span>
-
-                    <select id="verified_{{ $item->extplayer }}" style="display:none" class="w-100">
-                        <option value="1">Yes</option>
-                        <option value="0">No</option>
-                    </select>
-                </td>
-
-                <td>
-                    <span id="firstname_{{ $item->extplayer }}_display">{{ $item->nama_lengkap }}</span>
-                    <input type="text" id="firstname_{{ $item->extplayer }}" name="player_firstname" class="w-100"
-                        style="display:none" value="{{ $item->nama_lengkap }}">
-                </td>
-
-                <td> <span id="mobile_{{ $item->extplayer }}_display">{{ $item->no_hp }}</span>
-                    <input type="mobile" id="mobile_{{ $item->extplayer }}" class="w-100" style="display:none"
-                        value="{{ $item->no_hp }}">
-                    <input class="w-100" id="old_mobile_{{ $item->extplayer }}" type="text" name="old_mobile"
-                        value="{{ $item->no_hp }}" hidden>
-                </td>
-                <td class="text-middle"> {{ $item->nama_bank }} </td>
-                <td class="text-middle"> {{ number_format($item->balance,2) }} </td>
-                <td class="text-middle"> {{ $item->created_at ? date('Y-m-d H:i:s', strtotime($item->created_at)) : ''
-                    }} </td>
-                <td class="text-middle"> {{ $item->first_deposit_date ? date('Y-m-d H:i:s',
-                    strtotime($item->first_deposit_date)) : '' }} </td>
-                <td class="text-middle"> {{ $item->last_deposit_date ? date('Y-m-d H:i:s',
-                    strtotime($item->last_deposit_date)) : '' }} </td>
-                <td class="text-middle"> {{ $item->last_login_time ? date('Y-m-d H:i:s',
-                    strtotime($item->last_login_time)) : '' }} </td>
-                <td class="text-middle"> {{ $item->last_login_ip }} </td>
-                <td class="text-middle"> {{ $item->remark }} </td>
-                <td>
-                    <a href="javascript:void(0)" class="waves-effect waves-light btn btn-warning btn-sm btn-rounded"
-                        onclick="edit_button(this, `{{ $item->extplayer }}` )"> &#9998; </a>
-
-                    <div style="display:none">
-                        <a href="javascript:void(0)"
-                            class=" waves-effect waves-light btn btn-success btn-sm btn-rounded"
-                            onclick="submit_button(this , `{{ $item->extplayer }}` )"> &#10004; </a>
-                        <a href="javascript:void(0)"
-                            class="cancel_button waves-effect waves-light btn btn-danger btn-sm btn-rounded"
-                            onclick="cancel_button(this, `{{ $item->extplayer }}` )"> &#10007; </a>
-
-                    </div>
-                    <a href="javascript:void(0)" data-toggle="modal"
-                        class="waves-effect waves-light btn btn-sm btn-block btn-rounded"
-                        onclick="showModal('account_password_edit?id={{ $item->id }}&agent_id={{ $item->extplayer }}')">Edit
-                        Password</a>
-                </td>
-
-            </tr>
+                </tr>
             @endforeach
         </tbody>
 
@@ -169,9 +180,59 @@
         transform: translate(-50%, -50%);
     }
 </style>
+
+<div class="modal" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-dark" id="exampleModalLongTitle">Balance settings
+                    <b id="username_modal"></b>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <form id="balance_edit" method="post" action="/member_details/balance_settings">
+                @csrf
+                <input type="hidden" name="id" id="balance_edit_id" value="">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="text-dark float-left">Action</label>
+                        <select class="form-control" name="type">
+                            <option value="1">Deposit</option>
+                            <option value="2">Withdraw</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="text-dark float-left">Amount</label>
+                        <input type="text" class="form-control" name="amount" placeholder="Enter Amount"
+                            onkeypress='isNum(event)' required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success btn-submit-ball">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    function balance_settings(e, id, username) {
+        $('#exampleModalCenter').modal('show');
+        $('#username_modal').text(username);
+        $('#balance_edit_id').val(id);
+    }
+</script>
+
 <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
 <script>
     $(document).ready(function() {
+
         var agent = `{{ auth()->user()->agent_id }}`;
         var searching = true
         var columns = [];
@@ -217,7 +278,8 @@
 
 
     function edit_button(e, id) {
-        $(`#suspend_${id} , #status_${id} , #firstname_${id} , #lastname_${id} , #lock_${id}, #mobile_${id} , #old_mobile_${id} , #password_${id}, #verified_${id}`).show();
+        $(`#suspend_${id} , #status_${id} , #firstname_${id} , #lastname_${id} , #lock_${id}, #mobile_${id} , #old_mobile_${id} , #password_${id}, #verified_${id}`)
+            .show();
         $(`#suspend_${id}_display , #status_${id}_display , #firstname_${id}_display , #lastname_${id}_display , #lock_${id}_display ,#mobile_${id}_display , #password_${id}_display, #verified_${id}_display `)
             .hide();
         $(e).hide();
